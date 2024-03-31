68852b09095c05e1d8cbd6cd58fe2fdbdef0feac
A.Lapshin <polick@mail.ru>
2024-03-31 15:26:23 +0200

    Initial Environment for Tests

--- src/test/java/com/example/postgres/PostgresApplicationTests.java
+++ src/test/java/com/example/postgres/PostgresApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.postgres;
 
+import com.example.postgres.user.entity.UserEntity;
+import com.example.postgres.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PostgresApplicationTests {
+	@Autowired
+	private UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void repositoryTest() {
+		UserEntity user = UserEntity.builder()
+				.firstName("11")
+				.lastName("22")
+				.build();
+		user = userRepository.save(user);
+
+		UserEntity check = userRepository.findById(user.getId()).get();
+
+		assert check.getId().equals(user.getId());
 	}
 
 }
