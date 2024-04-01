a3ac1f1a1daf1701889fd185df9bc159244b47bb
A.Lapshin <polick@mail.ru>
2024-04-01 14:27:31 +0200

    Final Tests

--- src/main/java/com/example/postgres/user/dto/request/EditUserRequest.java
+++ src/main/java/com/example/postgres/user/dto/request/EditUserRequest.java
@@ -1,11 +1,13 @@
 // db-18:PUT/edit user
 package com.example.postgres.user.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 // @Setter
+@Builder
 public class EditUserRequest {
     private Long id;
     private String firstName;
--- src/test/java/com/example/postgres/WebTests.java
+++ src/test/java/com/example/postgres/WebTests.java
@@ -1,5 +1,8 @@
 package com.example.postgres;
 
+import com.example.postgres.user.dto.request.CreateUserRequest;
+import com.example.postgres.user.dto.request.EditUserRequest;
+import com.example.postgres.user.dto.response.UserResponse;
 import com.example.postgres.user.entity.UserEntity;
 import com.example.postgres.user.repository.UserRepository;
 import com.example.postgres.user.routes.UserRoutes;
@@ -10,10 +13,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -35,5 +42,93 @@ public class WebTests {
         mockMvc.perform(get(UserRoutes.BY_ID, user.getId().toString()).contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+    @Test
+    void createTest() throws Exception {
+        CreateUserRequest request = CreateUserRequest.builder()
+                .firstName("createUser")
+                .lastName("createUser")
+                .build();
+        mockMvc.perform(
+          post(UserRoutes.CREATE)
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .content(objectMapper.writeValueAsString(request))
+        )
+                .andDo(print())
+                .andExpect(content().string(containsString("createUser")));
+    }
+    @Test
+    void findByIdTest() throws Exception {
+        UserEntity user = UserEntity.builder()
+                .firstName("findById")
+                .lastName("findById")
+                .build();
+
+        userRepository.save(user);
+
+        mockMvc.perform(
+                get(UserRoutes.BY_ID, user.getId()).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("findById"))
+                );
+    }
+    @Test
+    void findById_NotFound_Test() throws Exception {
+        mockMvc.perform(
+                        get(UserRoutes.BY_ID, 12345).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+    @Test
+    void updateTest() throws Exception {
+        UserEntity user = UserEntity.builder()
+                .firstName("first")
+                .lastName("last")
+                .build();
+        userRepository.save(user);
+
+        EditUserRequest request = EditUserRequest.builder()
+                .id(user.getId())
+                .firstName("updateUser")
+                .lastName("updateUser")
+                .build();
+
+        mockMvc.perform(put(UserRoutes.BY_ID, user.getId().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        ).andDo(print())
+                .andExpect(content().string(containsString("updateUser")));
+    }
+    @Test
+    void deleteTest() throws Exception {
+        UserEntity user = UserEntity.builder()
+                .firstName("first")
+                .lastName("last")
+                .build();
+        userRepository.save(user);
+
+        mockMvc.perform(delete(UserRoutes.BY_ID, user.getId()))
+                .andDo(print())
+                .andExpect(status().isOk());
+        assert userRepository.findById(user.getId()).isEmpty();
+    }
+    @Test
+    void searchTest() throws Exception {
+        List<UserResponse> result = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            UserEntity user = UserEntity.builder()
+                    .firstName("userName_" + i)
+                    .lastName("userName_" + i)
+                    .build();
+            user = userRepository.save(user);
+            result.add(UserResponse.of(user));
+        }
+        mockMvc.perform(get(UserRoutes.SEARCH)
+                .param("size", "1000").contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(result)));
     }
 }
